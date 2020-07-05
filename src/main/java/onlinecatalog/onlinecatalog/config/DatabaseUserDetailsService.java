82358b4b60efcd108914e62b959b4e2daf21c18b
SerbanConstantin <serban.g.constantin@gmail.com>
2020-07-05 11:49:15 +0300

    create

---
+++ src/main/java/onlinecatalog/onlinecatalog/config/DatabaseUserDetailsService.java
@@ -0,0 +1,31 @@
+package onlinecatalog.onlinecatalog.config;
+
+import lombok.Getter;
+import onlinecatalog.onlinecatalog.config.CustomUserDetails;
+import onlinecatalog.onlinecatalog.model.User;
+import onlinecatalog.onlinecatalog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public DatabaseUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException(username));
+        return new CustomUserDetails(user);
+    }
+}
---
+++ src/main/java/onlinecatalog/onlinecatalog/model/User.java
@@ -0,0 +1,32 @@
+package onlinecatalog.onlinecatalog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
+// TODO: to be sure that username is unique
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // start from 1 on each table
+
+    private Integer userId;
+
+    private String username; // nickname
+    private String emailAddress;
+
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Role role;
+
+}
