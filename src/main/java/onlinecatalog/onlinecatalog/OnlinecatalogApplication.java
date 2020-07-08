c593e01b5a0fbab1b43589b81e759b0224e0e8a9
SerbanConstantin <serban.g.constantin@gmail.com>
2020-07-08 21:42:16 +0300

    user validation

--- src/main/java/onlinecatalog/onlinecatalog/OnlinecatalogApplication.java
+++ src/main/java/onlinecatalog/onlinecatalog/OnlinecatalogApplication.java
@@ -29,7 +29,7 @@ public class OnlinecatalogApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         try {
-            userRepository.deleteById(1);
+            userRepository.deleteById(3);
         } catch (Exception ex) {
 
         }
