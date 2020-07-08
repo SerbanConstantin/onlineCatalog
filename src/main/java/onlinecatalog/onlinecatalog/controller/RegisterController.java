b8a2eba71fcadc74d3046230ae8508884de3cd2e
SerbanConstantin <serban.g.constantin@gmail.com>
2020-07-08 20:51:21 +0300

    user validation

--- src/main/java/onlinecatalog/onlinecatalog/controller/RegisterController.java
+++ src/main/java/onlinecatalog/onlinecatalog/controller/RegisterController.java
@@ -5,6 +5,7 @@ import onlinecatalog.onlinecatalog.model.User;
 import onlinecatalog.onlinecatalog.repository.PendingUserRepository;
 import onlinecatalog.onlinecatalog.repository.UserRepository;
 import onlinecatalog.onlinecatalog.service.RandomStringGenerator;
+import onlinecatalog.onlinecatalog.service.SendGridEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class RegisterController {
@@ -26,6 +29,9 @@ public class RegisterController {
 
     @Autowired
     private RandomStringGenerator randomStringGenerator;
+
+    @Autowired
+    private SendGridEmailService sendGridEmailService;
 
     @GetMapping("/register")
     public String registerUser() {
@@ -44,15 +50,28 @@ public class RegisterController {
         user.setPassword(encoder().encode(password));
         user.setUsername(username);
 
-        userRepository.save(user);
+
         PendingUser pendingUser = new PendingUser();
         String activationCode = randomStringGenerator.getAlphaNumericString(20);
         pendingUser.setActivationCode(activationCode);
-
+        sendGridEmailService.sendHTML("buhaidebalta.15@gmail.com",
+                user.getEmailAddress(), "Please confirm account",
+                randomStringGenerator.linkCreator(activationCode, "https://online-school-catalog-sc.herokuapp.com/"));
         pendingUser.setUser(user);
 
         return "redirect:/login";
 
+    }
+
+    @GetMapping("/userValidation")
+    public String validateUser(String activationCode) {
+       Optional<PendingUser> optional = pendingUserRepository.findByActivationCode(activationCode);
+       if (optional.isPresent()){
+           PendingUser pendingUser = optional.get();
+           userRepository.save(pendingUser.getUser());
+           pendingUserRepository.delete(pendingUser);
+       }
+        return "security/login";
 
     }
 }
--- src/main/java/onlinecatalog/onlinecatalog/repository/PendingUserRepository.java
+++ src/main/java/onlinecatalog/onlinecatalog/repository/PendingUserRepository.java
@@ -3,6 +3,9 @@ package onlinecatalog.onlinecatalog.repository;
 import onlinecatalog.onlinecatalog.model.PendingUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PendingUserRepository extends JpaRepository<PendingUser, Integer> {
 
+    Optional<PendingUser> findByActivationCode(String activationCode);
 }
